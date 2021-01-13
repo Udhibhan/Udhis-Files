672f511d9a12aaafae8632fd6eaadfc3edde8c0a
Udhibhan <75685145+Udhibhan@users.noreply.github.com>
2021-01-13 15:39:34 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,32 @@
+class Bird{
+    constructor(x, y){
+        var options = {
+            //isStatic: true,
+            restitution: 1.5,
+            friction: 1,
+            density: 0.5
+        }
+        this.body = Bodies.rectangle(x,y,50,50,options);
+        this.width = 50;
+        this.height = 50;
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        pos.x = mouseX;
+        pos.y = mouseY;
+        var angle = this.body.angle;
+        angleMode(RADIANS);
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(3);
+        stroke("blue");
+        fill("green");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+};
